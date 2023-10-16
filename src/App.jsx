8381e4102fdf296d49b1d903c807d091ffcf8d20
Geoff George <geoffgeorgethommana@gmail.com>
2023-10-16 19:41:08 +0530

    all

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="container">
+      <Navbar/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import './navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+
+      <div className='logo'>
+        ServCL+
+      </div>
+      <ul>
+        <li>
+          About us
+        </li>
+        <li>
+          News
+        </li>
+        <li>
+          Contact us
+        </li>
+      </ul>
+
+      <div className='Contact'>
+        <h3>Having trouble ?</h3>
+        <button>Get help</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
